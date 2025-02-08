92d8c4aeb67ff8061fa35efc4cdb6eea0a372e3a
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2025-02-08 21:13:35 +0000

    fix clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,11 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}  appearance={{
+            layout: {
+                unsafe_disableDevelopmentModeWarnings: true,
+            },
+        }}>
             <html lang="en" suppressHydrationWarning>
             <body className={`${poppins.variable} antialiased`}>
             <ClientOnly>
