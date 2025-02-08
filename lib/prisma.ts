b3f278c6d3e28d62d5010ae905467ab0dced9d67
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2025-02-08 21:09:09 +0000

    fix build process

--- lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import {PrismaClient} from "@prisma/client";
-
-
-const globalForPrisma = globalThis as unknown as {prisma: PrismaClient}
-
-export const prisma = new PrismaClient() || globalForPrisma.prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
